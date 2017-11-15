0f7201a79843ef98a639f802f27d7327e57545cd
Truman Lahr <trumanlahr@gmail.com>
2017-11-15 15:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
+
+function appendKitten(name) {
+  const newKit = kittens
+  newKit.push(name)
+  return newKit
+}
+
+function prependKitten(name) {
+  const newKit = kittens
+  newKit.unshift(name)
+  return newKit
+}
+
+function removeLastKitten() {
+  const newKit = kittens
+  newKit.pop()
+  return newKit
+}
+
+function removeFirstKitten() {
+  const newKit = kittens
+  newKit.shift()
+  return newKit
+}
